52867f0f1347de5e61ef002fb28f18f071488a6a
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-11 18:56:38 -0500

    feat: draft article loading & parsing

--- src/boat.ts
+++ src/boat.ts
@@ -1,5 +1,7 @@
 import { com } from '@earth-app/ocean';
 import { Context } from 'hono';
+import { deflate } from 'pako';
+import { Bindings } from './types';
 
 export type Article = {
 	title: string;
@@ -17,15 +19,46 @@ export type Article = {
 	};
 };
 
-export async function findArticles(query: string, c: Context, limit: number = 3) {
-	com.earthapp.ocean.boat.Scraper.setApiKey('PubMed', c.env.NCBI_API_KEY);
+let HAS_PUBMED_API_KEY = false;
+export async function findArticles(
+	query: string,
+	c: Context<{ Bindings: Bindings }>,
+	limit: number = 3
+) {
+	if (!HAS_PUBMED_API_KEY) {
+		com.earthapp.ocean.boat.Scraper.setApiKey('PubMed', c.env.NCBI_API_KEY);
+		HAS_PUBMED_API_KEY = true;
+	}
 
 	const res = await com.earthapp.ocean.boat.searchAllAsPromise(
 		com.earthapp.ocean.boat.Scraper.Companion,
 		query,
 		limit
 	);
-	const results = res.asJsReadonlyArrayView().map(async (item) => JSON.parse(item.toJson()));
+	const results = res
+		.asJsReadonlyArrayView()
+		.map(async (item) => JSON.parse(item.toJson()) as Article);
 
 	return await Promise.all(results);
+}
+
+export async function createArticles(
+	query: string,
+	c: Context<{ Bindings: Bindings }>,
+	limit: number = 3
+) {
+	const articles = await findArticles(query, c, limit);
+	const compressed = deflate(JSON.stringify(articles));
+
+	const baseName = query.replace(/\s+/g, '_');
+	const fileName = `cloud/boat/${encodeURIComponent(baseName)}.json.gz`;
+
+	const exists = await c.env.R2.head(fileName);
+	if (exists) {
+		console.log(`File ${fileName} already exists in R2, deleting before upload.`);
+		await c.env.R2.delete(fileName);
+	}
+	await c.env.R2.put(fileName, compressed, {
+		httpMetadata: { contentType: 'application/gzip' }
+	});
 }
--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,12 @@ import { Ai, D1Database, Fetcher } from '@cloudflare/workers-types';
 
 export type Bindings = {
 	DB: D1Database;
+	R2: R2Bucket;
 	AI: Ai;
 	ASSETS: Fetcher;
 
 	ADMIN_API_TOKEN: string;
+	NCBI_API_KEY: string;
 };
 
 export type ActivityData = {
